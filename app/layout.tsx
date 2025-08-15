ae2cfa73d1d091dae68aafae1622f2461d977e19
Jinhyeok Choi <jinhyeokchoi@Jinhyeokui-MacBookAir.local>
2025-08-16 04:13:40 +0900

    정보수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,18 @@ export const metadata: Metadata = {
   description: "창의적이고 감동적인 연극을 선보이는 극단 큰강입니다.",
   generator: "v0.app",
   icons: { icon: `${prefix}/favicon.ico` },
+    openGraph: {
+    title: "극단 큰강",
+    description: "창의적이고 감동적인 연극을 선보이는 극단 큰강입니다.",
+    images: [
+      {
+        url: `${prefix}/logo-blue.png`, // public 폴더에 og-image.png 파일을 두세요
+        width: 1200,
+        height: 630,
+        alt: "극단 큰강 대표 이미지",
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
